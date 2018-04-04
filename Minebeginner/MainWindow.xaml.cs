956d9207471b2a0e77c48719ab2c758575e6d4f5
mob-a <34401954+mob-a@users.noreply.github.com>
2018-04-04 21:55:51 +0900

    update

--- Minebeginner/MainWindow.xaml.cs
+++ Minebeginner/MainWindow.xaml.cs
@@ -19,157 +19,78 @@ namespace Minebeginner
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public class Column
-    {
-        public List<Cell> Cells { get; set; }
-        public Column()
-        {
-            Cells = new List<Cell>();
-        }
-    }
-    public class Cell : INotifyPropertyChanged
-    {
-        // http://blog.livedoor.jp/morituri/archives/54652766.html
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public int ColumnID { get; set; }
-        public int RowID { get; set; }
-        public string ID { get; set; }
-        public Boolean HasBomb { get; set; }
-        public Boolean Opened { get; set; }
-        public string View { get; set; }
-        public Cell(int column_id, int row_id, Boolean has_bomb)
-        {
-            ColumnID = column_id;
-            RowID = row_id;
-            HasBomb = has_bomb;
-            ID = ColumnID + "/" + RowID;
-            Opened = false;
-
-            View = "";
-        }
-
-        public void Open(int around_bombs)
-        {
-            Opened = true;
-            if (HasBomb)
-            {
-                View = "★";
-
-                var window = new BombMessageWindow();
-                window.Show();
-            }
-            else
-            {
-                View = around_bombs.ToString();
-            }
-            OnPropertyChanged("View");
-        }
-
-        protected void OnPropertyChanged(string name)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(name));
-            }
-        }
-    }
-
-
     public partial class MainWindow : Window
     {
-        public List<Column> columns;
-        public int RowNum;
-        public int ColumnNum;
-        public int BombNum;
+        private MineBoard mineboard;
+        private Boolean end;
 
         public MainWindow()
         {
             InitializeComponent();
-
-            RowNum = 20;
-            ColumnNum = 15;
-            BombNum = 30;
-
-            var size = RowNum * ColumnNum;
-            int[] nums = new int[size];
-            for (var i = 0; i < size; i++)
-            {
-                nums[i] = i;
-            }
-            int[] shuffled = nums.OrderBy(i => Guid.NewGuid()).ToArray();
-            int[] bombIds = new int[BombNum];
-            for (var j = 0; j < BombNum; j++)
-            {
-                bombIds[j] = shuffled[j];
-            }
-
-            columns = new List<Column>();
-            for (int column_id = 0; column_id < ColumnNum; column_id++)
-            {
-                var column = new Column();
-
-                for (int row_id = 0; row_id < RowNum; row_id++)
-                {
-                    Boolean has_bomb = bombIds.Contains(column_id * RowNum + row_id);
-                    column.Cells.Add(new Cell(column_id, row_id, has_bomb));
-                }
-                columns.Add(column);
-            }
-            datalistitems.DataContext = columns;
+            InitializeBoard(15,20, 10);
         }
 
-        public Cell GetCell(int column_id, int row_id)
+        void InitializeBoard(int column_num, int row_num, int bomb_num)
         {
-            return columns[column_id].Cells[row_id];
+            end = false;
+            mineboard = new MineBoard(column_num, row_num, bomb_num);
+            datalistitems.DataContext = mineboard.Columns;
+            closed_cell_counter.Text = mineboard.ClosedSafeCellNum.ToString();
+            this.Width =column_num * 20 + 36;
+            this.Height= row_num * 20 + 102;
         }
-        public void SetCell(int column_id, int row_id, string view)
-        {
-            var cell = new Cell(column_id, row_id, true);
 
-            cell.View = view;
-            columns[column_id].Cells[row_id] = cell;
-        }
-        public int GetAroundBombs(int column_id, int row_id)
-        {
-            int num = 0;
-            for (int c = column_id - 1; c <= column_id + 1; c++)
-            {
-                if (c < 0 || c >= ColumnNum)
-                {
-                    continue;
-                }
-                for (int r = row_id - 1; r <= row_id + 1; r++)
-                {
-                    if (c == column_id && r == row_id)
-                    {
-                        continue;
-                    }
-                    if (r < 0 || r >= RowNum)
-                    {
-                        continue;
-                    }
-                    if (GetCell(c, r).HasBomb)
-                    {
-                        num++;
-                    }
-                }
-            }
-            return num;
-        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            if (end)
+            {
+                return;
+            }
             var button = (Button)sender;
             var tag = button.Tag;
             var columnid_rowid = tag.ToString().Split('/').Select(numstr => int.Parse(numstr)).ToArray();
 
             var column_id = columnid_rowid[0];
             var row_id = columnid_rowid[1];
-            var cell = GetCell(column_id, row_id);
-            var around_bombs = GetAroundBombs(column_id, row_id);
-            cell.Open(around_bombs);
+            var result = mineboard.Open(column_id, row_id);
+            if (result == OpenResult.BOMB)
+            {
+                end = true;
+                var window = new BombMessageWindow();
+                window.Show();
+            }
+            else if (result == OpenResult.SUCCESS)
+            {
+                closed_cell_counter.Text = mineboard.ClosedSafeCellNum.ToString();
+                if (mineboard.ClosedSafeCellNum == 0)
+                {
+                    var cwindow = new ClearMessageWindow();
+                    cwindow.Show();
+                    end = true;
+                }
+            }
+        }
+
+
+        private void Button_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (end)
+            {
+                return;
+            }
+            var button = (Button)sender;
+            var tag = button.Tag;
+            var columnid_rowid = tag.ToString().Split('/').Select(numstr => int.Parse(numstr)).ToArray();
+
+            var column_id = columnid_rowid[0];
+            var row_id = columnid_rowid[1];
+
+            mineboard.ReverseFlag(column_id, row_id);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            InitializeBoard(5,4,3);
         }
     }
 
---
+++ Minebeginner/MineBoard.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Minebeginner
+{
+    enum OpenResult { SUCCESS, BOMB, CANNOTOPEN };
+    class MineBoard
+    {
+        public List<Column> Columns { get; }
+        public int ColumnNum { get; }
+        public int RowNum { get; }
+        public int BombNum { get; }
+        public int ClosedSafeCellNum { get; set; }
+
+        public MineBoard(int column_num, int row_num, int bomb_num)
+        {
+            ColumnNum = column_num;
+            RowNum = row_num;
+            BombNum = bomb_num;
+
+            ClosedSafeCellNum = ColumnNum * RowNum - BombNum; ;
+
+            int size = RowNum * ColumnNum;
+            int[] nums = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                nums[i] = i;
+            }
+            int[] shuffled = nums.OrderBy(i => Guid.NewGuid()).ToArray();
+            int[] bombIds = new int[BombNum];
+            for (int j = 0; j < BombNum; j++)
+            {
+                bombIds[j] = shuffled[j];
+            }
+
+            Columns = new List<Column>();
+            for (int column_id = 0; column_id < ColumnNum; column_id++)
+            {
+                var column = new Column();
+
+                for (int row_id = 0; row_id < RowNum; row_id++)
+                {
+                    Boolean has_bomb = bombIds.Contains(column_id * RowNum + row_id);
+                    column.Cells.Add(new Cell(column_id, row_id, has_bomb));
+                }
+                Columns.Add(column);
+            }
+
+            for (int column_id = 0; column_id < ColumnNum; column_id++)
+            {
+                for (int row_id = 0; row_id < RowNum; row_id++)
+                {
+                    var cell = GetCell(column_id, row_id);
+                    if (!cell.HasBomb)
+                    {
+                        cell.AroundBombs = GetAroundBombs(column_id, row_id);
+                    }
+                }
+            }
+        }
+
+        public Cell GetCell(int column_id, int row_id)
+        {
+            return Columns[column_id].Cells[row_id];
+        }
+
+        public OpenResult Open(int column_id, int row_id)
+        {
+            var cell = GetCell(column_id, row_id);
+            if (cell.HasFlag || cell.Opened)
+            {
+                return OpenResult.CANNOTOPEN;
+            }
+
+            cell.Open();
+            if (cell.HasBomb)
+            {
+                return OpenResult.BOMB;
+            }
+            else
+            {
+                ClosedSafeCellNum--;
+                if (cell.AroundBombs == 0)
+                {
+                    for (int c = column_id - 1; c <= column_id + 1; c++)
+                    {
+                        if (c < 0 || c >= ColumnNum)
+                        {
+                            continue;
+                        }
+                        for (int r = row_id - 1; r <= row_id + 1; r++)
+                        {
+                            if (c == column_id && r == row_id)
+                            {
+                                continue;
+                            }
+                            if (r < 0 || r >= RowNum)
+                            {
+                                continue;
+                            }
+                            var acell = this.GetCell(c, r);
+                            if (!acell.Opened)
+                            {
+                                Open(c, r);
+                            }
+                        }
+                    }
+                }
+                return OpenResult.SUCCESS;
+            }
+        }
+        public void ReverseFlag(int column_id, int row_id)
+        {
+            var cell = GetCell(column_id, row_id);
+            if (!cell.Opened)
+            {
+                cell.ReverseFlag();
+            }
+        }
+        public int GetAroundBombs(int column_id, int row_id)
+        {
+            int num = 0;
+            for (int c = column_id - 1; c <= column_id + 1; c++)
+            {
+                if (c < 0 || c >= ColumnNum)
+                {
+                    continue;
+                }
+                for (int r = row_id - 1; r <= row_id + 1; r++)
+                {
+                    if (c == column_id && r == row_id)
+                    {
+                        continue;
+                    }
+                    if (r < 0 || r >= RowNum)
+                    {
+                        continue;
+                    }
+                    if (GetCell(c, r).HasBomb)
+                    {
+                        num++;
+                    }
+                }
+            }
+            return num;
+        }
+    }
+    class Column
+    {
+        public List<Cell> Cells { get; set; }
+        public Column()
+        {
+            Cells = new List<Cell>();
+        }
+    }
+    public class Cell : INotifyPropertyChanged
+    {
+        // http://blog.livedoor.jp/morituri/archives/54652766.html
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int ColumnID { get; set; }
+        public int RowID { get; set; }
+        public string ID { get; set; }
+        public Boolean HasBomb { get; set; }
+        public Boolean Opened { get; set; }
+        public string View { get; set; }
+        public int AroundBombs { get; set; }
+        public Boolean HasFlag { get; set; }
+        public Cell(int column_id, int row_id, Boolean has_bomb)
+        {
+            ColumnID = column_id;
+            RowID = row_id;
+            HasBomb = has_bomb;
+            ID = ColumnID + "/" + RowID;
+            Opened = false;
+            AroundBombs = -1;
+            View = "";
+            HasFlag = false;
+        }
+        public void ReverseFlag()
+        {
+            if (Opened)
+            {
+                return;
+            }
+
+            if (HasFlag)
+            {
+                View = "";
+                HasFlag = false;
+                OnPropertyChanged("View");
+            }
+            else
+            {
+                View = "■";
+                HasFlag = true;
+                OnPropertyChanged("View");
+            }
+        }
+
+        public void Open()
+        {
+            if (Opened)
+            {
+                return;
+            }
+            Opened = true;
+            if (HasBomb)
+            {
+                View = "★";
+
+            }
+            else
+            {
+                View = AroundBombs.ToString();
+            }
+            OnPropertyChanged("View");
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
